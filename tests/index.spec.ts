49312ee242cd0981d69583558478bcb3952f68be
rwv <7891383+rwv@users.noreply.github.com>
2024-08-01 14:13:31 +0800

    test: improve vitest coverage to 100% (#26)
    * test: calculate SRI should throw an error if the file does not exist
    
    * test: workbox-add-integrity should transform manifest successfully with default options

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,6 @@
 import addIntegrity from "../src/index.js";
 import { expect, test, describe } from "vitest";
+import fs from "fs/promises";
 
 describe("workbox-add-integrity", () => {
   test("should transform manifest successfully using sha256", async () => {
@@ -16,5 +17,28 @@ describe("workbox-add-integrity", () => {
 
     const result = await hashFunction(manifest);
     expect(result.manifest[0].integrity).toBe(expectedIntegrity);
+  });
+
+  test("should transform manifest successfully with default options", async () => {
+    // mkdir dist and copy example.txt to dist
+    await fs.mkdir("dist", { recursive: true });
+    await fs.copyFile("tests/example.txt", "dist/example.txt");
+
+    const expectedIntegrity =
+      "sha256-8pvGSp03MrS5A1El/bMoX1tkVXeO3KckFGceDKOy4N4=";
+    const hashFunction = addIntegrity();
+    const manifest = [
+      {
+        url: "example.txt",
+        revision: null,
+        size: 0,
+      },
+    ];
+
+    const result = await hashFunction(manifest);
+    expect(result.manifest[0].integrity).toBe(expectedIntegrity);
+
+    // cleanup
+    await fs.rm("dist", { recursive: true });
   });
 });
